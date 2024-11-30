b0f600ef0610babf5c1f4efb6d9de53f4ccf8532
PauloRodri29 <rodriguesjp29@gmail.com>
2024-11-30 15:58:17 -0300

    Criada e finalizada a splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:scoref_manager/core/routers/app_routers.dart';
 import 'package:scoref_manager/widgets/home/home_page.dart';
 
 void main() {
-    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  //   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp( MaterialApp(home: const MyApp(),debugShowCheckedModeBanner: false, initialRoute: AppRoutes.i.initial,),);
 }
 
